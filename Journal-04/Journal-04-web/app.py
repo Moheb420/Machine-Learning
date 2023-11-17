37c8a967c195bf9950093392f60baab0ebf3a8c9
Andreas <201908525@post.au.dk>
2023-11-17 10:27:07 +0100

    ny model til web

--- Journal-04/Journal-04-web/app.py
+++ Journal-04/Journal-04-web/app.py
@@ -1,13 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from sklearn.svm import SVR
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 import pandas as pd
 import os
+import joblib
+from flask_wtf import FlaskForm
+from wtforms import SelectField
+
 
 app = Flask(__name__)
+app.secret_key = "your_secret_key"  # Change this to a secret key
 
-# Load dataset
+svm_math = joblib.load("../svm_math_model.pkl")
+svm_reading = joblib.load("../svm_reading_model.pkl")
+svm_writing = joblib.load("../svm_writing_model.pkl")
+mlp_math = joblib.load("../mlp_math_model.pkl")
+mlp_reading = joblib.load("../mlp_reading_model.pkl")
+mlp_writing = joblib.load("../mlp_writing_model.pkl")
+
+datapath = os.path.join("../../Journal-04", "", "")
+dataset = pd.read_csv(datapath + "StudentsPerformance.csv")
+
+# Preprocess the dataset
+X = dataset[["gender", "parental level of education", "test preparation course"]]
+
+X = pd.get_dummies(
+    X, columns=["gender", "parental level of education", "test preparation course"]
+)
+
+""" # Load dataset
 datapath = os.path.join("../../Journal-04", "", "")
 dataset = pd.read_csv(datapath + "StudentsPerformance.csv")
 
@@ -30,7 +52,47 @@ svm_reading.fit(X_train_reading, y_reading_train)
 
 X_train_writing, X_test_writing, y_writing_train, y_writing_test = train_test_split(X, y_writing, test_size=0.2, random_state=42)
 svm_writing = SVR(kernel='linear')
-svm_writing.fit(X_train_writing, y_writing_train)
+svm_writing.fit(X_train_writing, y_writing_train) """
+
+
+""" @app.route("/", methods=["GET", "POST"])
+def home():
+    math_prediction = None
+    reading_prediction = None
+    writing_prediction = None
+
+    if request.method == "POST":
+        # Get user input data from the form
+        gender = request.form.get("gender")
+        education = request.form.get("education")
+        prep_course = request.form.get("prep_course")
+        model = request.form.get("model")
+
+        # Convert user input into a DataFrame
+        user_data = {
+            "gender_female": [1 if gender == "female" else 0],
+            "gender_male": [1 if gender == "male" else 0],
+            f"parental level of education_{education}": [1],
+            "test preparation course_completed": [
+                1 if prep_course == "completed" else 0
+            ],
+            f"model{model}": [1],
+        }
+        user_df = pd.DataFrame(user_data)
+        user_df = user_df.reindex(columns=X.columns, fill_value=0)
+
+        # Perform predictions for math, reading, and writing scores
+        math_prediction = svm_math.predict(user_df)
+        reading_prediction = svm_reading.predict(user_df)
+        writing_prediction = svm_writing.predict(user_df)
+
+    return render_template(
+        "index.html",
+        math_prediction=math_prediction,
+        reading_prediction=reading_prediction,
+        writing_prediction=writing_prediction,
+    ) """
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -43,23 +105,37 @@ def home():
         gender = request.form.get("gender")
         education = request.form.get("education")
         prep_course = request.form.get("prep_course")
+        model = request.form.get("model")
 
         # Convert user input into a DataFrame
         user_data = {
-            'gender_female': [1 if gender == "female" else 0],
-            'gender_male': [1 if gender == "male" else 0],
+            "gender_female": [1 if gender == "female" else 0],
+            "gender_male": [1 if gender == "male" else 0],
             f"parental level of education_{education}": [1],
-            'test preparation course_completed': [1 if prep_course == "completed" else 0]
+            "test preparation course_completed": [
+                1 if prep_course == "completed" else 0
+            ],
         }
         user_df = pd.DataFrame(user_data)
         user_df = user_df.reindex(columns=X.columns, fill_value=0)
 
-        # Perform predictions for math, reading, and writing scores
-        math_prediction = svm_math.predict(user_df)
-        reading_prediction = svm_reading.predict(user_df)
-        writing_prediction = svm_writing.predict(user_df)
+        # Perform predictions based on the selected model
+        if model == "SVM":
+            math_prediction = svm_math.predict(user_df)
+            reading_prediction = svm_reading.predict(user_df)
+            writing_prediction = svm_writing.predict(user_df)
+        elif model == "MLP":
+            math_prediction = mlp_math.predict(user_df)
+            reading_prediction = mlp_reading.predict(user_df)
+            writing_prediction = mlp_writing.predict(user_df)
 
-    return render_template("index.html", math_prediction=math_prediction, reading_prediction=reading_prediction, writing_prediction=writing_prediction)
+    return render_template(
+        "index.html",
+        math_prediction=math_prediction,
+        reading_prediction=reading_prediction,
+        writing_prediction=writing_prediction,
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
